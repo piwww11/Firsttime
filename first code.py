d6f581584d917f60e02fc39c1adab5f8807742dd
piwww11 <revvy135@gmail.com>
2025-12-30 20:03:08 +0700

    Initial commit

---
+++ first code.py
@@ -0,0 +1,7 @@
+nama = input("masukan nama:")
+umur = int(input("masukan umur:"))
+hobi = input("masukan hobi:")
+
+print(f"Halo {nama}! Umur lu {umur} tahun ya?")
+print(f"Hobi {hobi}? keren lah")
+print(f"Semangat terus and keep smile!")
